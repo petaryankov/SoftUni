b69ecc5ed5748539a9743e6398184df86790eb63
Petar Yankov <yankov.petar4@gmail.com>
2022-01-20 11:12:05 +0200

    other way of printer queue

---
+++ 03JavaAdvancedSept2021/Labs and Exercises/01StacksAndQueuesLab/P05PrinterQueue.java
@@ -0,0 +1,28 @@
+package day1.StacksAndQueuesLab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P05PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        ArrayDeque<String> printerQueue = new ArrayDeque<>();
+        while (!input.equals("print")) {
+            if (input.equals("cancel")) {
+                if (printerQueue.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    System.out.println("Canceled " + printerQueue.poll());
+                }
+            } else {
+                printerQueue.offer(input);
+            }
+
+            input = scanner.nextLine();
+        }
+        printerQueue.forEach(System.out::println);
+    }
+}
