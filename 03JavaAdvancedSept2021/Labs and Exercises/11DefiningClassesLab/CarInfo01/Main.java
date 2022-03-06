9d4b011d1add3006b726ec2cd6fec1aaad21e883
Petar Yankov <yankov.petar4@gmail.com>
2022-03-06 16:04:34 +0200

    CarInfo added

---
+++ 03JavaAdvancedSept2021/Labs and Exercises/11DefiningClassesLab/CarInfo01/Main.java
@@ -0,0 +1,24 @@
+package day11.DefiningClassesLab.CarInfo01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        while (n-- > 0) {
+            String input = scanner.nextLine();
+            String carBrand = input.split("\\s+")[0];
+            String carModel = input.split("\\s+")[1];
+            int carHp = Integer.parseInt(input.split("\\s+")[2]);
+            Car car = new Car(carBrand, carModel, carHp);
+
+            System.out.printf("The car is: %s %s - %d HP.\n",
+                    car.getBrand(), car.getModel(), car.getHorsePower());
+        }
+
+    }
+}
