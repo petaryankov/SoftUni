9ec685beb150b11c9becc75ea8d7a75214403fc3
Petar Yankov <yankov.petar4@gmail.com>
2022-03-07 08:09:37 +0200

    OpinionPoll added

---
+++ 03JavaAdvancedSept2021/Labs and Exercises/12DefiningClassesExercises/P1OpinionPoll/Person.java
@@ -0,0 +1,24 @@
+package day12.DefiningClassesExercises.P1.OpinionPoll;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " - " + this.age;
+    }
+}
