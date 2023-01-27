3110cf8895debeac84c78b1c741e9890eae8c00d
Petar Yankov <yankov.petar4@gmail.com>
2023-01-27 13:36:14 +0200

    Delete 05JS_AdvancedMay2022/02 Syntax, Functions and Statements Exercise directory
    duplicate folder

--- 05JS_AdvancedMay2022/02 Syntax, Functions and Statements Exercise/01 Fruit.js
+++
@@ -1,7 +0,0 @@
-function solve(fruit, weight, price) {
-    let weightInKg = weight / 1000;
-    let money = (weightInKg * price).toFixed(2);
-    
-    console.log(`I need $${money} to buy ${weightInKg.toFixed(2)} kilograms ${fruit}.`);
-}
-solve('apple', 1563, 2.35);
