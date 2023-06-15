16dcd22e653b4bf9b9a044b1d183c53e86b83196
Petar Yankov <yankov.petar4@gmail.com>
2023-06-15 12:10:31 +0300

    new exam preparation folder with files

---
+++ 06MySQL_May2023/Exams/Exam15October2022/08TopFromMenu.sql
@@ -0,0 +1,9 @@
+SELECT 
+    id, name, COUNT(op.order_id) AS count
+FROM
+    products p
+        JOIN
+    orders_products op ON p.id = op.product_id
+GROUP BY id
+HAVING count >= 5
+ORDER BY count DESC , name;
---
+++ 06MySQL_May2023/Exams/Exam15October2022/09Availability.sql
@@ -0,0 +1,19 @@
+SELECT 
+    t.id,
+    t.capacity,
+    COUNT(oc.client_id) AS 'count_clients',
+    CASE
+        WHEN capacity > COUNT(oc.client_id) THEN 'Free seats'
+        WHEN capacity = COUNT(oc.client_id) THEN 'Full'
+        ELSE 'Extra seats'
+    END AS availability
+FROM
+    tables t
+        JOIN
+    orders o ON t.id = o.table_id
+        JOIN
+    orders_clients oc ON o.id = oc.order_id
+WHERE
+    floor = 1
+GROUP BY t.id
+ORDER BY t.id DESC;
