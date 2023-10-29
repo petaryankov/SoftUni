fde12274156a77407356bba7c87cf4c22cbdc8ea
Petar Yankov <yankov.petar4@gmail.com>
2023-10-29 21:16:42 +0200

    new files added

---
+++ 10SpringAdvancedOct2023/books/src/main/java/softuni/books/model/dto/AuthorDTO.java
@@ -0,0 +1,13 @@
+package softuni.books.model.dto;
+
+public class AuthorDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
