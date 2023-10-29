c3a668ddaf13169f29a1f6b2348c145db0878dd1
Petar Yankov <yankov.petar4@gmail.com>
2023-10-29 21:49:12 +0200

    update

---
+++ 10SpringAdvancedOct2023/books/src/main/java/web/BooksRestController.java
@@ -0,0 +1,26 @@
+package web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import softuni.books.model.dto.BookDTO;
+import softuni.books.service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BooksRestController {
+
+    private final BookService bookService;
+
+    public BooksRestController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+}
