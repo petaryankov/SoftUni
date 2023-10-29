f5b21d47074c45c24418871f5cedcc979dfec161
Petar Yankov <yankov.petar4@gmail.com>
2023-10-29 21:47:55 +0200

    new method added

--- 10SpringAdvancedOct2023/books/src/main/java/softuni/books/service/BookService.java
+++ 10SpringAdvancedOct2023/books/src/main/java/softuni/books/service/BookService.java
@@ -1,4 +1,9 @@
 package softuni.books.service;
 
+import softuni.books.model.dto.BookDTO;
+
+import java.util.List;
+
 public interface BookService {
+    List<BookDTO> getAllBooks();
 }
--- 10SpringAdvancedOct2023/books/src/main/java/softuni/books/service/BookServiceImpl.java
+++ 10SpringAdvancedOct2023/books/src/main/java/softuni/books/service/BookServiceImpl.java
@@ -1,7 +1,41 @@
 package softuni.books.service;
 
 import org.springframework.stereotype.Service;
+import softuni.books.model.dto.AuthorDTO;
+import softuni.books.model.dto.BookDTO;
+import softuni.books.model.entity.Author;
+import softuni.books.model.entity.Book;
+import softuni.books.repository.BookRepository;
+
+import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public List<BookDTO> getAllBooks() {
+        return bookRepository
+                .findAll()
+                .stream()
+                .map(BookServiceImpl::mapBookToDTO)
+                .toList();
+    }
+
+    private static BookDTO mapBookToDTO(Book book) {
+        BookDTO aBook = new BookDTO();
+        AuthorDTO authorDTO = new AuthorDTO();
+        authorDTO.setName(book.getAuthor().getName());
+
+        aBook.setId(book.getId());
+        aBook.setTitle(book.getTitle());
+        aBook.setIsbn(book.getIsbn());
+        aBook.setAuthor(authorDTO);
+
+        return aBook;
+    }
 }
