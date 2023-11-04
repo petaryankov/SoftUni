c910813d240836cf497033e7e5c07e052744302e
Petar Yankov <yankov.petar4@gmail.com>
2023-11-04 23:48:19 +0200

    updated

--- 10SpringAdvancedOct2023/books-js-client/.idea/app.js
+++ 10SpringAdvancedOct2023/books-js-client/.idea/app.js
@@ -4,11 +4,42 @@ loadBooksButton.addEventListener('click', loadBooks);
 
 function loadBooks() {
 
-    let booksContainer = document.getElementById('books-container')
+    let booksContainer = document.getElementById('books-container');
+    booksContainer.innerHTML = '';
 
     fetch('http://localhost:8080/api/books')
         .then(response => response.json())
         .then(json => json.forEach(book => {
             console.log(book);
+
+            let bookRow = document.createElement('tr');
+
+            let titleCol = document.createElement('td');
+            let authorCol = document.createElement('td');
+            let isbnCol = document.createElement('td');
+            let actionCol = document.createElement('td');
+
+            titleCol.textContent = book.title;
+            authorCol.textContent = book.author.name;
+            isbnCol.textContent = book.isbn;
+
+            let deleteBookBtn = document.createElement('button');
+            deleteBookBtn.textContent = 'DELETE';
+
+            deleteBookBtn.addEventListener('click', deleteBook);
+
+            actionCol.appendChild(deleteBookBtn);
+
+            bookRow.appendChild(titleCol);
+            bookRow.appendChild(authorCol);
+            bookRow.appendChild(isbnCol);
+            bookRow.appendChild(actionCol);
+
+            booksContainer.append(bookRow);
+
         }));
+}
+
+function deleteBook() {
+    alert("deleted")
 }
