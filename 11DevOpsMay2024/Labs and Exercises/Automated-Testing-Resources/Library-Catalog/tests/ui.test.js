11d0fd6cf16de04e5f34d7951f81bebaffe84ec8
Petar Yankov <yankov.petar4@gmail.com>
2024-06-06 23:09:51 +0300

    Test for register and login btn is added

--- 11DevOpsMay2024/Labs and Exercises/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
+++ 11DevOpsMay2024/Labs and Exercises/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
@@ -18,3 +18,41 @@ test('Verify "All Books" link is visible', async({page}) => {
 
 
 });
+
+test('Verify "Login" button is visible', async({page}) => {
+    
+    //open the application
+    await page.goto('http://localhost:3000');
+
+    //local page toolbar
+    await page.waitForSelector('nav.navbar');
+
+    //get login button
+    const loginButton = await page.$('a[href = "/login"]');
+
+    //Verify the element is visible
+    const isElementVisible = await loginButton.isVisible();
+
+    expect(isElementVisible).toBe(true);
+
+
+});
+
+test('Verify "Register" button is visible', async({page}) => {
+    
+    //open the application
+    await page.goto('http://localhost:3000');
+
+    //local page toolbar
+    await page.waitForSelector('nav.navbar');
+
+    //get login button
+    const registerButton = await page.$('a[href = "/register"]');
+
+    //Verify the element is visible
+    const isElementVisible = await registerButton.isVisible();
+
+    expect(isElementVisible).toBe(true);
+
+
+});
