377a25efe9936955971642f741f971857c0940aa
Petar Yankov <yankov.petar4@gmail.com>
2024-06-06 22:55:43 +0300

    tests for all books added

--- 11DevOpsMay2024/Labs and Exercises/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
+++ 11DevOpsMay2024/Labs and Exercises/Automated-Testing-Resources/Library-Catalog/tests/ui.test.js
@@ -1,6 +1,20 @@
 const {test, expect} = require('@playwright/test');
 
 test('Verify "All Books" link is visible', async({page}) => {
-
+    
+    //open the application
     await page.goto('http://localhost:3000');
+
+    //local page toolbar
+    await page.waitForSelector('nav.navbar');
+
+    //get all books link
+    const allBooksLink = await page.$('a[href = "/catalog"]');
+
+    //Verify the element is visible
+    const isElementVisible = await allBooksLink.isVisible();
+
+    expect(isElementVisible).toBe(true);
+
+
 });
